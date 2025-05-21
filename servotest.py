ce1849a7c6747a2371c6acea9a2837025d371933
JPON24 <jpon24@shaw.ca>
2025-05-21 10:40:08 -0600

    Create servotest.py

---
+++ servotest.py
@@ -0,0 +1,102 @@
+"""
+Copyright MIT
+MIT License
+bwsix RC101 - Fall 2023
+
+File Name: demo.py
+
+Title: Demo RACECAR program
+
+Purpose: To verify that basic RACECAR functions work properly and the student has set up
+the system correctly to run Python scripts with the RACECAR start/update paradigm in the
+Unity simulator.
+
+Expected Outcome: Terminal output and RACECAR movement occurs when buttons are pressed
+- When the "A" button is pressed, print a message to the terminal window
+- When the "B" button is pressed, RACECAR moves forward and to the right
+"""
+
+# screen - 480, 640 (y,x)
+# 110 degree fov width
+
+# TEST THIS CODE ON THE COLOR CAMERA SANDBOX ENVIRONMENT
+# SHOULD SEEK OUT A BLUE CONE AND DRIVE TOWARDS IT, DECELERATING FROM A CERTAIN DISTANCE
+
+########################################################################################
+# Imports
+########################################################################################
+
+#racecar sim navigation.py
+
+import sys
+from gpiozero import PWMOutputDevice
+import time
+
+sys.path.insert(0, '../library')
+import racecar_core
+import racecar_utils as rc_utils
+
+########################################################################################
+# Global variables
+########################################################################################
+
+
+
+# size of color matrix
+
+# Declare any global variables here
+
+
+########################################################################################
+# Functions
+########################################################################################
+new_servo = PWMOutputDevice(5)
+# [FUNCTION] The start function is run once every time the start button is pressed
+def start():
+    new_servo = PWMOutputDevice(5)
+
+    # This tells the car to begin at a standstill
+    rc.drive.stop()
+
+
+# [FUNCTION] After start() is run, this function is run once every frame (ideally at
+# 60 frames per second or slower depending on processing speed) until the back button
+
+def update():
+    global new_servo
+    new_servo.value = 0
+    time.sleep(1)
+    new_servo.value = 1
+
+def servo_test():
+    global new_servo
+
+    new_servo.value = 0
+    time.sleep(1)
+    
+    new_servo.value = 0.5
+    time.sleep(1)
+    
+    new_servo.value = 1
+    time.sleep(1)
+
+def update_slow():
+    """
+    After start() is run, this function is run at a constant rate that is slower
+    than update().  By default, update_slow() is run once per second
+    """
+    # This prints a message every time that the right bumper is pressed during
+    # a call to update_slow.  If we press and hold the right bumper, it
+    # will print a message once per second
+
+    if rc.controller.is_down(rc.controller.Button.RB):
+        print("The right bumper is currently down (update_slow)")
+
+
+########################################################################################
+# DO NOT MODIFY: Register start and update and begin execution
+########################################################################################
+
+if __name__ == "__main__":
+    rc.set_start_update(start, update, update_slow)
+    rc.go()
